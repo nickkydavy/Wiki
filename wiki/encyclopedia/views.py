22ca1a41f3bf05cc4dd322284045ec7c9e081438
nickkydavy <p.nimwattana@gmail.com>
2021-05-23 14:27:16 +0700

    Added entry function

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
+markdown = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def showEntry(request, title):
+    return render(request, "encyclopedia/entry.html",{
+        "content": markdown.convert(util.get_entry(title))
+    })
